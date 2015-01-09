1b288c632f501b5ac99b1656e3f70233b6025097
jixiuf <jixiuf@gmail.com>
2015-01-09 15:11:37 +0800

    add test

--- data_buffer_test.go
+++ data_buffer_test.go
@@ -1,6 +1,7 @@
 package databuffer
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -15,5 +16,28 @@ func TestGetMatchSize(t *testing.T) {
 
 	assert.Equal(t, SLOT_INIT_SIZE*SLOT_RATIO/100, getMatchSize(SLOT_INIT_SIZE+1))
 	assert.Equal(t, SLOT_INIT_SIZE*SLOT_RATIO/100, getMatchSize(SLOT_INIT_SIZE*SLOT_RATIO/100))
+
+}
+func TestGetBuffer(t *testing.T) {
+	data := GetBuffer(SLOT_GIP)
+	assert.Equal(t, len(data), SLOT_GIP)
+	assert.True(t, cap(data) == SLOT_GIP)
+
+	data2 := GetBuffer(SLOT_GIP - 1)
+	assert.Equal(t, len(data2), SLOT_GIP-1)
+	assert.True(t, cap(data2) == SLOT_GIP)
+	fmt.Printf("这里打印的地址应该是不同的%p,%p\n", data, data2)
+
+}
+func TestPutBuffer(t *testing.T) {
+	data := GetBuffer(SLOT_GIP)
+	assert.Equal(t, len(data), SLOT_GIP)
+	assert.True(t, cap(data) == SLOT_GIP)
+	PutBuffer(data)
+
+	data2 := GetBuffer(SLOT_GIP - 1)
+	assert.Equal(t, len(data2), SLOT_GIP-1)
+	assert.True(t, cap(data2) == SLOT_GIP)
+	fmt.Printf("这里打印的地址应该是相同的%p,%p\n", data, data2)
 
 }
